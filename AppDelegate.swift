a74332da8af41f4f7f25b82ccc0ebc6214bf1dc6
Angel Davila <a6519150005@utch.edu.mx>
2021-12-09 09:01:11 -0700

    App configuration

---
+++ AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  iOSCalculator
+//
+//  Created by Edwin on 03/12/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        setupView()
+        
+        return true
+    }
+    
+    // MARK: Private methods
+    
+    private func setupView(){
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
+
